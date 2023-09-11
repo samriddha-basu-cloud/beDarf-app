394dcccce2ecac267501338280ff34551d3fb190
samriddha-basu-cloud <sb2899@srmist.edu.in>
2023-09-12 04:26:07 +0530

    setup of MVC

---
+++ bedarf/android/app/src/main/kotlin/com/example/bedarf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bedarf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bedarf/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const beDarf());
+}
+
+class beDarf extends StatelessWidget {
+  const beDarf({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
